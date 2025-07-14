baba79d97bd01114681d38dfa9498a22d50ae70c
mrirakib04 <mdrakibulislam11928@gmail.com>
2025-07-14 15:37:10 +0600

    header added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,6 +3,7 @@ import { GoBell } from "react-icons/go";
 import { FaSearch } from "react-icons/fa";
 import { useState } from "react";
 import { PiListBold } from "react-icons/pi";
+import bg from "./../assets/img/cat-big.png";
 
 const Header = () => {
   const [showNav, setShowNav] = useState(false);
@@ -11,7 +12,7 @@ const Header = () => {
   };
 
   return (
-    <div className="w-full flex flex-col items-center">
+    <div className="w-full flex flex-col items-center h-[480px] sm:h-[570px] md:h-[650px] lg:h-[750px]">
       <nav className="w-full bg-[#FFF9F4] flex items-center justify-between md:px-10 px-5 py-5">
         <a className="flex items-center gap-1" href="./../../index.html">
           <img className="w-8" src={logo} alt="logo" />
@@ -81,6 +82,25 @@ const Header = () => {
           </ul>
         )}
       </nav>
+      <div className="w-full md:py-20 py-10 md:px-10 px-5 relative">
+        <div className="lg:w-5/12 md:w-9/12 sm:w-9/12 flex flex-col gap-5 items-start">
+          <h2 className="lg:text-5xl md:text-4xl text-3xl">
+            Pamper Your Pet, <br /> Because They Deserve the Bes
+          </h2>
+          <p className="text-gray-700">
+            Discover a world of treats, toys, and essentials handpicked for your
+            furry friends. Shop now and make tails wag with joy!
+          </p>
+          <button className="px-6 py-1 rounded-lg text-lg font-medium border border-yellow-300 bg-yellow-300 hover:bg-black hover:text-white duration-300">
+            Find a Pet
+          </button>
+        </div>
+        <img
+          className="-z-10 px-3 w-11/12 absolute md:-bottom-50 sm:-bottom-32 -bottom-10"
+          src={bg}
+          alt="bg"
+        />
+      </div>
     </div>
   );
 };
